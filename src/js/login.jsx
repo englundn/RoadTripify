9c05160d4bf9ffcee27724c64f2fb2db2f14288e
Andrew <anxsaiyuki@gmail.com>
2016-10-20 14:47:58 -0700

    add express and nodemon for server

---
+++ src/js/login.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Components
+var Header = require('../components/header');
+
+//Mixins
+var API = require('../mixins/APImixin');
+
+var App = React.createClass({
+	mixins: [API],
+
+	render() {
+		return (
+			<div>
+			<Header />
+			<div>Login page</div>
+
+			</div>
+		)
+	}
+})
+
+ReactDOM.render(<App />, document.getElementById('App'))
