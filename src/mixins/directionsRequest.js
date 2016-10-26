eaca5dae1af48ccf424117010ad0a7523d4f0dcf
Nick Englund <englundn@gmail.com>
2016-10-26 14:06:21 -0700

    Google maps data now broken up into half hour chunks.

--- src/mixins/directionsRequest.js
+++ src/mixins/directionsRequest.js
@@ -7,13 +7,10 @@ var googleMapsClient = require('@google/maps').createClient({
 // https://developers.google.com/maps/documentation/javascript/directions
 var getDirections = function(origin, destination, departureTime) {
   
-  // console.log(googleMapsClient);
-  // console.log(googleMapsClient.directions);
-
   var request = {
     'origin': origin,
     'destination': destination,
-    'departure_time': new Date(departureTime),
+    'departure_time': departureTime,
     'mode': 'driving',
     'traffic_model': 'best_guess',
     'optimize': true,
@@ -22,19 +19,61 @@ var getDirections = function(origin, destination, departureTime) {
   googleMapsClient.directions(request, function(error, response) {
     if (error) {
       console.error('error', error);
-      // callback(error, null);
     } else {
-      // console.log('successfully retrieved directions');
-      // callback(null, response);
       var currentTime = departureTime;
+
+      //generates an array of objects containing time and location for each Google Maps direction
       var wayPoints = response.json.routes[0].legs[0].steps.map(step => {
-        currentTime += step.duration.value;
-        return {time: new Date(currentTime),
-                lat: step.end_location.lat,
-                lng: step.end_location.lng};
+        currentTime += step.duration.value * 1000;
+        return {
+          time: currentTime,
+          lat: step.end_location.lat,
+          lng: step.end_location.lng
+        };
       });
-      console.log(wayPoints);
-      return wayPoints;     
+
+
+      //generates an array of times half an hour apart, starting at the start time of the trip
+      //this is for generating weather data
+      var timeInHalfHourIncrements = [wayPoints[0].time];
+      while (departureTime < currentTime) {
+        timeInHalfHourIncrements.push(departureTime += 30 * 60 * 1000);
+      }
+
+
+      //generates a location/time object at a middle time that occurs between a start object and an end object
+      var interpolator = function(start, end, middleTime) {
+        var deltaTime = (middleTime - start.time) / (end.time - start.time);
+        var deltaLat = (end.lat - start.lat) * deltaTime;
+        var deltaLng = (end.lng - start.lng) * deltaTime;
+        return {
+          time: middleTime,
+          lat: start.lat + deltaLat,
+          lng: start.lng + deltaLng
+        };
+      };
+
+      //uses the time in half hour increments to generate interpolated coordinates from Google maps direction
+      var halfHourWayPoints = [wayPoints[0]];
+      var currentWayPointIndex = 0;
+      for (var i = 1; i < timeInHalfHourIncrements.length; i++) {
+        while (timeInHalfHourIncrements[i] > wayPoints[currentWayPointIndex].time && wayPoints[currentWayPointIndex + 1]) {
+          currentWayPointIndex++;
+        }
+        var newPoint = interpolator(wayPoints[currentWayPointIndex - 1],
+                                    wayPoints[currentWayPointIndex],
+                                    timeInHalfHourIncrements[i]);
+        halfHourWayPoints.push(newPoint);
+      }
+
+      //Converts time properties into date objects
+      halfHourWayPoints = halfHourWayPoints.map(function(waypoint) {
+        waypoint.time = new Date(waypoint.time);
+        return waypoint;
+      });
+
+      console.log(halfHourWayPoints);
+      return halfHourWayPoints;
     }
   });
 
