4d9b75ee42f81b0ecd8a96c76d003e423bf43f39
szhou1 <zhousteve3@gmail.com>
2016-10-26 17:52:15 -0700

    Add env var for callback

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new SpotifyStrategy({
   clientID: appKey,
   clientSecret: appSecret,
-  callbackURL: 'http://roadtripify1.herokuapp.com/callback'
+  callbackURL: process.env.CALLBACK || 'http://localhost:8888/callback'
 },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
