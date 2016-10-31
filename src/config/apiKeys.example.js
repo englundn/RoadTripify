68dc177e661bfc1299f33d6f3b657a9ec0b83e9e
Nick Englund <englundn@gmail.com>
2016-10-31 12:15:29 -0700

    Updated apiKeys.example.js to accomodate multiple weather API keys.

--- src/config/apiKeys.example.js
+++ src/config/apiKeys.example.js
@@ -1,8 +1,15 @@
 //Replace 'INSERT API KEY' with your own API keys.
 //spotifyAccessToken is only necessary for testing.
+//the accuWeather API has a limited number of API calls per day
+//To avoid hitting the limit, we made multiple API keys and use them at random
 
 module.exports = {
   googleMapsApiKey: 'INSERT API KEY',
-  accuWeatherApiKey: 'INSERT API KEY',
+  accuWeatherApiKey: [
+    'INSERT API KEY',
+    'INSERT API KEY',
+    'INSERT API KEY',
+    'INSERT API KEY',
+  ][ Math.floor(Math.random() * 4) ],
   spotifyAccessToken: 'INSERT ACCESS TOKEN'
 };
